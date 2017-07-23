ef3ebcedce3e7b2b5a18e0d6e4235983f56792c4
Saul Toscano <toscano.saul@gmail.com>
2017-07-23 00:53:39 -0400

    fixed a bug in parallel

--- stratified_bayesian_optimization/lib/parallel.py
+++ stratified_bayesian_optimization/lib/parallel.py
@@ -51,7 +51,6 @@ class Parallel(object):
 
         results = {}
         for key in arguments.keys():
-            results[key] = jobs[key].get()
             try:
                 results[key] = jobs[key].get()
             except Exception as e:
