54a81c997c45c995413d2cafa0ccc2997262319a
Saul Toscano <toscano.saul@gmail.com>
2017-07-28 21:05:48 -0400

    added ei

--- problems/arxiv/scripts/run_year_data.py
+++ problems/arxiv/scripts/run_year_data.py
@@ -2,14 +2,22 @@ from problems.arxiv.process_raw_data import ProcessRawData
 
 import argparse
 
+from stratified_bayesian_optimization.initializers.log import SBOLog
+
+logger = SBOLog(__name__)
+
 
 if __name__ == '__main__':
-    # python -m problems.arxiv.scripts.run_year_data
+    # python -m problems.arxiv.scripts.run_year_data --month 1
     parser = argparse.ArgumentParser()
     parser.add_argument('month', help='e.g. 23')
     args = parser.parse_args()
     month = args.month
 
     files = ProcessRawData.generate_filenames_month(2016, int(args.month))
+
+    logger.info("Files to be processed: ")
+    logger.info(files)
+
     ProcessRawData.get_click_data(
         files,"problems/arxiv/data/2016_%s_processed_data.json" % month)
