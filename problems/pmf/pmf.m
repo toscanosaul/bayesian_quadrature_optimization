bff18c7768f2f19654d933d407a9d02a508bd5a6
Saul Toscano <toscano.saul@gmail.com>
2017-08-07 21:06:29 -0400

    pmf in matlabl

---
+++ problems/pmf/pmf.m
@@ -0,0 +1,15 @@
+function [error] = pmf(num_p, num_m, epsilon, lambda, maxepoch, num_feat, train_vec, probe_vec);
+
+rand('state',0);
+randn('state',0);
+
+momentum = 0.8;
+num_batches = 9;
+
+w1_M1 = 0.1*randn(num_m, num_feat);
+w1_P1 = 0.1*randn(num_p, num_feat);
+w1_M1_inc = zeros(num_m, num_feat);
+w1_P1_inc = zeros(num_p, num_feat);
+
+mean_rating = mean(train_vec(:,3));
+error mean_rating
