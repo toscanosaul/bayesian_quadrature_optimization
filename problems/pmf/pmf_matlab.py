d344f0f6824827429c9d263bbeb5dcaec2ea243b
Saul Toscano <toscano.saul@gmail.com>
2017-08-07 23:24:47 -0400

    pmf_matlab

--- problems/pmf/pmf_matlab.py
+++ problems/pmf/pmf_matlab.py
@@ -1,3 +1,9 @@
 import matlab.engine
 
 eng = matlab.engine.start_matlab()
+
+def PMF(num_user, num_item, train, val, epsilon=50, lamb=0.01, maxepoch=50, num_feat=10,
+        l_rating=1, u_rating=5):
+
+    return -1.0 * eng.pmf(
+        num_user, num_item, train, val, epsilon, lamb,  maxepoch, num_feat, l_rating, u_rating)
