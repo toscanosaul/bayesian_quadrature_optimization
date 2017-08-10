f7dd89ad98acdd8d3cff5c517ec356222bdfd2dc
Saul Toscano <toscano.saul@gmail.com>
2017-08-10 01:54:31 -0400

    add path to matlab server

--- problems/pmf/pmf_matlab.py
+++ problems/pmf/pmf_matlab.py
@@ -1,6 +1,9 @@
 import matlab.engine
+import os
 
 eng = matlab.engine.start_matlab()
+cwd = os.getcwd()
+eng.addpath(cwd + '/problems/pmf/')
 
 def PMF(num_user, num_item, train, val, epsilon=50, lamb=0.01, maxepoch=50, num_feat=10,
         l_rating=1, u_rating=5):
