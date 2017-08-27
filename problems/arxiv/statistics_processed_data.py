727d723d2a4f0c2dfea4e0122ae7ef9ec209d349
Saul Toscano <toscano.saul@gmail.com>
2017-08-27 02:46:07 -0400

    categories of papers

--- problems/arxiv/statistics_processed_data.py
+++ problems/arxiv/statistics_processed_data.py
@@ -13,11 +13,13 @@ logger = SBOLog(__name__)
 class StatisticsProcessedData(object):
 
     _name_file_ = 'problems/arxiv/data/{year}_{month}_processed_data.json'.format
+    _name_file_categories = 'problems/arxiv/data/papers.json'
     _name_file_final = 'problems/arxiv/data/{year}_{month}_top_users.json'.format
+    _papers_path = '/data/json/idcat/'
 
     @classmethod
     def top_users_papers(cls, year, month, n_entries=100, different_papers=20, top_n=5000,
-                         n_users=None):
+                         n_users=None, only_assign_categories=True):
         """
         Returns the users that accessed to at least n_entries papers, and at least different_papers
         were different and were in the top_n papers in the month of the year.
@@ -57,6 +59,12 @@ class StatisticsProcessedData(object):
             rank_papers[paper_ls[index]] = n_papers[index]
 
         paper_ls = rank_papers.keys()
+
+        cls.assign_categories(paper_ls)
+
+        if only_assign_categories:
+            return
+        
         rank_user = {}
 
         users_ls = []
@@ -101,7 +109,120 @@ class StatisticsProcessedData(object):
         logger.info('Number of papers is %d' % len(rank_papers))
         logger.info('Number of users is %d' % len(rank_user))
 
+
         return [rank_papers, rank_user]
+
+    @classmethod
+    def assign_categories(cls, list_papers):
+        """
+
+        :param list_papers: [str]
+        :return: {paper_name (str):  category (str)}
+        """
+        papers = {}
+        for paper in list_papers:
+
+            before_2007 = False
+            arxiv_id = paper
+
+            if '/' in arxiv_id:
+                 before_2007 = True
+                 index = arxiv_id.index('/')
+                 cat = arxiv_id[0: index]
+                 arxiv_id = arxiv_id[index + 1:]
+
+            if 'v' in arxiv_id:
+                index = arxiv_id.rfind('v')
+                arxiv_id = arxiv_id[0: index]
+
+            if not before_2007:
+                cat = cls.get_cats(arxiv_id, arxiv_id[0: 2], arxiv_id[2: 4])
+
+            papers[paper] = cat
+
+        JSONFile.write(papers, cls._name_file_categories)
+        return papers
+
+    @classmethod
+    def get_cats(cls, arxiv_id, year, month):
+          """
+          Get category of a file
+
+          :param arxiv_id: (str)
+          :param year: (str) e.g. '07', '10'
+          :param month: (str) e.g. '12', '02'
+          :return: str
+
+          """
+
+          filename = path.join(cls._papers_path, '20' + year)
+          date = year + month
+
+          cats = None
+
+          for day in xrange(1, 32):
+               if day < 10:
+                    date_ = date + '0' + str(day)
+               else:
+                    date_ = date + str(day)
+
+               date_ += '_idcat.json'
+               filename_ = path.join(filename, date_)
+
+
+               data = None
+               if path.exists(filename_):
+                    with open(filename_) as f:
+                         data = ujson.load(f)
+
+               if data is not None:
+                    for dicts in data['new']:
+                         if dicts['id'] == arxiv_id:
+                              cats = [a.lower() for a in dicts["cat"].split(":")]
+                              break
+
+               if cats is not None:
+                    return cats[0]
+
+          new_month = int(month) + 1
+
+          if new_month == 13:
+               new_month = 1
+               year = int(year) + 1
+               if year < 10:
+                    year = '0' + str(year)
+               else:
+                    year = str(year)
+
+          if new_month < 10:
+               new_month = '0' + str(new_month)
+          else:
+               new_month = str(new_month)
+
+          filename = path.join(cls._papers_path, '20' + year)
+
+          for day in xrange(1, 10):
+               date = year + new_month + '0' + str(day) + '_idcat.json'
+               filename_ = path.join(filename, date)
+
+               data = None
+               if path.exists(filename_):
+                    with open(filename_) as f:
+                         data = ujson.load(f)
+
+               if data is not None:
+                    for dicts in data['new']:
+                         if dicts['id'] == arxiv_id:
+                              cats = [a.lower() for a in dicts["cat"].split(":")]
+                              break
+
+               if cats is not None:
+                    return cats[0]
+
+          if cats is None:
+               logger.info("Couldn't find category of paper %s" % arxiv_id)
+
+          return cats
 
 
     def top_papers_year(self, n, year):
