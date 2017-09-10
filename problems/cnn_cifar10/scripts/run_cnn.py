6d68803e5daa8eff2fbbb0d3b8a33629714124e8
Saul Toscano <toscano.saul@gmail.com>
2017-09-09 22:25:35 -0400

    cnn with cuda

--- problems/cnn_cifar10/scripts/run_cnn.py
+++ problems/cnn_cifar10/scripts/run_cnn.py
@@ -12,11 +12,13 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('random_seed', help='e.g. 2')
+    parser.add_argument('n_epochs', help='e.g. 2')
 
     args = parser.parse_args()
     rs = int(args.random_seed)
+    n_epochs = int(args.n_epochs)
 
-    errors = train_nn(rs)
+    errors = train_nn(rs, n_epochs)
 
     directory = 'problems/cnn_cifar10/runs_random_seeds/' + 'rs_%d' % rs + '.json'
 
