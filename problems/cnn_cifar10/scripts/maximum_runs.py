3542b7e338e6857f4db75c17bec8ac9f5e732492
Saul Toscano <toscano.saul@gmail.com>
2017-09-09 23:28:17 -0400

    script to find maximum accuracy of cnn

--- problems/cnn_cifar10/scripts/maximum_runs.py
+++ problems/cnn_cifar10/scripts/maximum_runs.py
@@ -12,7 +12,7 @@ logger = SBOLog(__name__)
 
 if __name__ == '__main__':
     # Example usage:
-    # python -m problems.cnn_cifar10.maximum_runs 500 600
+    # python -m problems.cnn_cifar10.scripts.maximum_runs 500 600
 
     parser = argparse.ArgumentParser()
     parser.add_argument('min_rs', help='e.g. 500')
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     max_values = []
     for i in xrange(min_rs, max_rs):
         file_name = 'problems/cnn_cifar10/runs_random_seeds/' + 'rs_%d' % i + '.json'
-        if not os.path.exists(dir):
+        if not os.path.exists(file_name):
             continue
         data = JSONFile.read(file_name)
         max_values.append(data['test_error_images'])
