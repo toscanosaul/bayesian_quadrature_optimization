5d73302ab23a169a2fb22dd790d70748cc315d0f
Saul Toscano <toscano.saul@gmail.com>
2017-09-22 01:28:31 -0400

    added main cnn file

--- problems/cnn_cifar10/cnn.py
+++ problems/cnn_cifar10/cnn.py
@@ -79,26 +79,39 @@ def train_nn(random_seed, n_epochs=2, batch_size=4, lr=0.001, weight_decay=0,
     if testset is None:
         testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                             download=True, transform=transform)
+    if size_kernel % 2 == 0:
+        size_kernel += 1
 
     class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
+
             self.conv1 = nn.Conv2d(3, number_chanels_first, size_kernel)
+            dim_1 = (32 - size_kernel + 1) / 2
             self.pool = nn.MaxPool2d(2, 2)
             self.conv2 = nn.Conv2d(number_chanels_first, number_chanels_second, size_kernel)
+            dim_2 = (dim_1 - size_kernel + 1) / 2
+            self.total_dim = number_chanels_second * dim_2 * dim_2
             self.fc1 = \
-                nn.Linear(number_chanels_second * size_kernel * size_kernel, number_hidden_units)
+                nn.Linear(self.total_dim, number_hidden_units)
             self.fc2 = nn.Linear(number_hidden_units, 84)
             self.fc3 = nn.Linear(84, 10)
 
         def forward(self, x):
             x = self.pool(F.relu(self.conv1(x)))
             x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, self.num_flat_features(x))
+            x = x.view(-1, self.total_dim)
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = self.fc3(x)
             return x
+
+        def num_flat_features(self, x):
+            size = x.size()[1:]  # all dimensions except the batch dimension
+            num_features = 1
+            for s in size:
+                num_features *= s
+            return num_features
 
     net = Net()
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
