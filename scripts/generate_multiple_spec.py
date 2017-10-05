e39ba953ecd34bb2e61536f287c633eeab39b121
Saul Toscano <toscano.saul@gmail.com>
2017-10-05 04:49:20 -0400

    negative error in cnn

--- scripts/generate_multiple_spec.py
+++ scripts/generate_multiple_spec.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
                       [0, 1, 2, 3, 4]]]
     #old bounds: [[0.01, 1.01], [0.01, 2.1], [1, 21], [1, 201], [0, 1, 2, 3, 4]]
     n_training = [5]
-    random_seed = range(1, 20)
+    random_seed = range(1, 50)
     n_specs = len(random_seed)
     type_bounds = [[0, 0, 0, 0, 0, 0, 1]]
     x_domain = [[0, 1, 2, 3, 4, 5]]
@@ -93,10 +93,10 @@ if __name__ == '__main__':
     n_best_restarts_mc = [0]
     factr_mc = [1e12]
     maxiter_mc = [10]
-    n_restarts = [100]
-    n_best_restarts = [10]
+    n_restarts = [10]
+    n_best_restarts = [0]
     use_only_training_points = [True]
-    method_optimization = [MULTI_TASK_METHOD]
+    method_optimization = [SBO_METHOD]
     n_samples_parameters = [5]
     n_restarts_mean = [100]
     n_best_restarts_mean = [10]
