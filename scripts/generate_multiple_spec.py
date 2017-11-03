f0c7ee54fdcb6222e41e59f3e0cb1366de0266f2
Saul Toscano <toscano.saul@gmail.com>
2017-11-03 01:43:25 -0400

    sbo_vendor

--- scripts/generate_multiple_spec.py
+++ scripts/generate_multiple_spec.py
@@ -151,6 +151,7 @@ if __name__ == '__main__':
     maxepoch_mean = [20]
     threshold_sbo = [0.001]
     parallel_training = [False]
+    noises = [True]
 
     specs = SpecService.generate_dict_multiple_spec(
         n_specs=n_specs, problem_names=problem_name, dim_xs=dim_x, bounds_domain_xs=bounds_domain_x,
@@ -168,7 +169,7 @@ if __name__ == '__main__':
         n_restarts_means=n_restarts_mean, n_best_restarts_means=n_best_restarts_mean,
         n_best_restarts_mcs=n_best_restarts_mc, maxepochs=maxepoch, method_opt_mcs=method_opt_mc,
         n_samples_parameters_means=n_samples_parameters_mean, maxepoch_means=maxepoch_mean,
-        threshold_sbos=threshold_sbo, parallel_trainings=parallel_training,
+        threshold_sbos=threshold_sbo, parallel_trainings=parallel_training, noises=noises,
     )
 
     print ujson.dumps(specs, indent=4)
