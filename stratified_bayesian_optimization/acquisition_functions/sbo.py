2f9097f72587b59473cd260d5bb1f148e5a515d2
Saul Toscano <toscano.saul@gmail.com>
2017-12-25 06:52:51 -0500

    reduce momentum in sgd when we exit the domain

--- stratified_bayesian_optimization/acquisition_functions/sbo.py
+++ stratified_bayesian_optimization/acquisition_functions/sbo.py
@@ -1937,56 +1937,35 @@ class SBO(object):
 
         vect_gradient = optimal_solutions.get(ind_max)['gradient']
         if vect_gradient == 'unavailable' or np.any(np.isnan(vect_gradient)):
-            point_boundary = True
+            point = optimal_solutions.get(ind_max)['solution']
+
+            if not self.bq.task_continue and self.bq.separate_tasks:
+                task = point[-1]
+                point = point[0: len(point) - 1]
+
+            norm_point = np.sqrt(np.sum(point ** 2))
+            perturbation = norm_point * 1e-6
+            parameters_uniform = []
             for i in range(len(bounds)):
                 bound = bounds[i]
-                if bound[0] != point[i] and bound[1] != point[i]:
-                    point_boundary = False
-                    break
+                dist = point[i] - bound[0]
+                lb = min(perturbation, dist)
+                dist = bound[1] - point[i]
+                ub = min(perturbation, dist)
+                parameters_uniform.append([-lb, ub])
 
+            perturbation = []
+            for i in range(len(point)):
+                lb = parameters_uniform[i][0]
+                ub = parameters_uniform[i][1]
+                perturbation.append(np.random.uniform(lb, ub))
+            perturbation = np.array(perturbation)
+            point = point + perturbation
 
-            if not point_boundary:
-                new_points = DomainService.get_points_domain(
-                    100, self.bq.bounds, type_bounds=self.bq.type_bounds)
-                current_points = np.array(self.bq.gp.data['points'])
+            if not self.bq.task_continue and self.bq.separate_tasks:
+                point = np.concatenate((point, [task]))
 
-                if not self.bq.task_continue and self.bq.separate_tasks:
-                    current_points = current_points[:, 0:-1]
-
-                distances = Distances.dist_square_length_scale(
-                    np.ones(len(new_points[0])), new_points, current_points)
-                max_distances = np.min(distances, axis=1)
-
-                sort_dist_ind = sorted(range(len(max_distances)), key=lambda k: max_distances[k])
-
-                md = int(np.ceil(len(max_distances) / 2.0))
-                uq = int(np.ceil(len(max_distances) / 4.0))
-                lw = int(np.ceil(len(max_distances) / 8.0))
-
-                index_1 = 10 / 2
-                index_2 = 10 - index_1
-                index_1 = np.random.choice(range(uq, md), index_1, replace=False)
-                index_2 = np.random.choice(range(lw, uq), index_2, replace=False)
-
-                index_1 = [sort_dist_ind[t] for t in index_1]
-                index_2 = [sort_dist_ind[t] for t in index_2]
-                index = index_1 + index_2
-                i = np.random.randint(0, len(index))
-
-                if not self.bq.task_continue and self.bq.separate_tasks:
-                    task = optimal_solutions.get(ind_max)['solution'][-1]
-                    new_point = np.concatenate((new_points[i], [task]))
-                else:
-                    new_point = np.array(new_points[index[i]])
-            else:
-                if not self.bq.task_continue and self.bq.separate_tasks:
-                    task = optimal_solutions.get(ind_max)['solution'][-1]
-                    new_point = 0
-                else:
-                    new_point = 0
-
-
-            optimal_solutions.get(ind_max)['solution'] = new_point
+            optimal_solutions.get(ind_max)['solution'] = point
             optimal_solutions.get(ind_max)['gradient'] = 'unavailable'
 
         self.optimization_results.append(optimal_solutions.get(ind_max))
--- stratified_bayesian_optimization/lib/stochastic_gradient_descent.py
+++ stratified_bayesian_optimization/lib/stochastic_gradient_descent.py
@@ -33,18 +33,47 @@ def SGD(start, gradient, n, args=(), kwargs={}, bounds=None, learning_rate=0.1, 
     v = np.zeros(len(start))
     times_out_boundary = 0
 
-    for i in xrange(maxepoch):
+    for iteration in xrange(maxepoch):
         previous = point.copy()
         for j in xrange(n):
             gradient_ = gradient(point, *args, **kwargs)
+
             if gradient_ is np.nan:
                 norm_point = np.sqrt(np.sum(point ** 2))
                 perturbation = norm_point * 1e-6
-                point = point + np.random.uniform(-perturbation, perturbation, size=len(point))
+
+                if project:
+                    parameters_uniform = []
+                    for i in range(len(bounds)):
+                        bound = bounds[i]
+                        dist = point[i] - bound[0]
+                        lb = min(perturbation, dist)
+                        dist = bound[1] - point[i]
+                        ub = min(perturbation, dist)
+                        parameters_uniform.append([-lb, ub])
+                else:
+                    parameters_uniform = len(point) * [[-perturbation, perturbation]]
+
+                perturbation = []
+                for i in range(len(point)):
+                    lb = parameters_uniform[i][0]
+                    ub = parameters_uniform[i][1]
+                    perturbation.append(np.random.uniform(lb, ub))
+                perturbation = np.array(perturbation)
+                point = point + perturbation
                 gradient_ = gradient(point, *args, **kwargs)
-            v = momentum * v + learning_rate * gradient_
-            point -= v
+
+            v = momentum * v + gradient_
+            point -= learning_rate * v
+
+            in_domain = True
             if project:
+                for dim, bound in enumerate(bounds):
+                    if point[dim] < bound[0] or point[dim] > bound[1]:
+                        in_domain = False
+                        break
+
+            if project and not in_domain:
                 for dim, bound in enumerate(bounds):
                     if bound[0] is not None:
                         point[dim] = max(bound[0], point[dim])
@@ -56,6 +85,7 @@ def SGD(start, gradient, n, args=(), kwargs={}, bounds=None, learning_rate=0.1, 
                     if times_out_boundary > 1:
                         momentum = 0
                         learning_rate *= 0.5
+                        v = np.zeros(len(start))
 
         den_norm = (np.sqrt(np.sum(previous ** 2)))
 
