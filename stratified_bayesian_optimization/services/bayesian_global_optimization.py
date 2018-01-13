785c6ab4c33b843ddebd66f9ee4ca73966e28561
Saul Toscano <toscano.saul@gmail.com>
2018-01-13 04:29:44 -0500

    new specs of branin_sbo

--- stratified_bayesian_optimization/services/bayesian_global_optimization.py
+++ stratified_bayesian_optimization/services/bayesian_global_optimization.py
@@ -203,6 +203,9 @@ class BGO(object):
 
         :return: Objective
         """
+        if self.n_iterations <= 0:
+            return 0
+
         if optimize_only_posterior_mean:
             # only for noisless problems
             chosen_points = self.gp_model.data.copy()
