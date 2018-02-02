68426776f55ebcb415d969dbaefbafd73a4fcd30
Saul Toscano <toscano.saul@gmail.com>
2018-02-01 20:43:12 -0500

    more random seeds for branin sde

--- scripts/generate_multiple_spec.py
+++ scripts/generate_multiple_spec.py
@@ -484,59 +484,59 @@ if __name__ == '__main__':
 
 
     #branin-sde
-    # domain_random = [[0.25, 0.2], [0.25, 0.4], [0.25, 0.6], [0.25, 0.8],
-    #                           [0.5, 0.2], [0.5, 0.4], [0.5, 0.6], [0.5, 0.8],
-    #                           [0.75, 0.2], [0.75, 0.4], [0.75, 0.6], [0.75, 0.8]]
-    #
-    # dim_x = [2]
-    # bounds_domain_x = [[(0, 1), (0, 1)]]
-    # problem_name = ['branin']
-    # training_name = [None]
-    # type_kernel = [[MATERN52_NAME]]
-    # dimensions = [[4]]
-    # bounds_domain = [[[0, 1], [0, 1], [0.25, 0.5, 0.75], [0.2, 0.4, 0.6, 0.8]]]
-    # n_training = [12]
-    # random_seed = range(1, 501)
-    # n_specs = len(random_seed)
-    # type_bounds = [[0, 0, 1, 1]]
-    # x_domain = [[0, 1]]
-    # number_points_each_dimension = [[6, 6]]
-    # mle = [False]
-    # distribution = [WEIGHTED_UNIFORM_FINITE]
-    # parallel = [True]
-    # thinning = [10]
-    # n_burning = [500]
-    # max_steps_out = [1000]
-    # n_iterations = [46]
-    # same_correlation = [True]
-    # debug = [False]
-    # number_points_each_dimension_debug = [[10, 10, 10, 10, 10, 10]]
-    # monte_carlo_sbo = [True]
-    # n_samples_mc = [50]
-    # n_restarts_mc = [5]
-    # n_best_restarts_mc = [0]
-    # factr_mc = [1e12]
-    # maxiter_mc = [10]
-    # n_restarts = [10]
-    # n_best_restarts = [0]
-    # use_only_training_points = [True]
-    # method_optimization = [SDE_METHOD]
-    # n_samples_parameters = [5]
-    # n_restarts_mean = [10]
-    # n_best_restarts_mean = [0]
-    # method_opt_mc = [DOGLEG]
-    # maxepoch = [10]
-    # n_samples_parameters_mean = [20]
-    # maxepoch_mean = [20]
-    # threshold_sbo = [0.001]
-    # parallel_training = [False]
-    # noises = [False]
-    # n_sampless = [0]
-    # parameters_distributions = [{'weights': [0.0375, 0.0875, 0.0875, 0.0375, 0.0750, 0.1750,
-    #                                          0.1750, 0.0750, 0.0375, 0.0875, 0.0875, 0.0375],
-    #                              'domain_random': domain_random}]
-    # optimize_only_posterior_means = [False]
-    # start_optimize_posterior_means = [0]
+    domain_random = [[0.25, 0.2], [0.25, 0.4], [0.25, 0.6], [0.25, 0.8],
+                              [0.5, 0.2], [0.5, 0.4], [0.5, 0.6], [0.5, 0.8],
+                              [0.75, 0.2], [0.75, 0.4], [0.75, 0.6], [0.75, 0.8]]
+
+    dim_x = [2]
+    bounds_domain_x = [[(0, 1), (0, 1)]]
+    problem_name = ['branin']
+    training_name = [None]
+    type_kernel = [[MATERN52_NAME]]
+    dimensions = [[4]]
+    bounds_domain = [[[0, 1], [0, 1], [0.25, 0.5, 0.75], [0.2, 0.4, 0.6, 0.8]]]
+    n_training = [12]
+    random_seed = range(1, 701)
+    n_specs = len(random_seed)
+    type_bounds = [[0, 0, 1, 1]]
+    x_domain = [[0, 1]]
+    number_points_each_dimension = [[6, 6]]
+    mle = [False]
+    distribution = [WEIGHTED_UNIFORM_FINITE]
+    parallel = [True]
+    thinning = [10]
+    n_burning = [500]
+    max_steps_out = [1000]
+    n_iterations = [42]
+    same_correlation = [True]
+    debug = [False]
+    number_points_each_dimension_debug = [[10, 10, 10, 10, 10, 10]]
+    monte_carlo_sbo = [True]
+    n_samples_mc = [50]
+    n_restarts_mc = [5]
+    n_best_restarts_mc = [0]
+    factr_mc = [1e12]
+    maxiter_mc = [10]
+    n_restarts = [10]
+    n_best_restarts = [0]
+    use_only_training_points = [True]
+    method_optimization = [SDE_METHOD]
+    n_samples_parameters = [5]
+    n_restarts_mean  = [10]
+    n_best_restarts_mean = [0]
+    method_opt_mc = [DOGLEG]
+    maxepoch = [10]
+    n_samples_parameters_mean = [20]
+    maxepoch_mean = [20]
+    threshold_sbo = [0.001]
+    parallel_training = [False]
+    noises = [False]
+    n_sampless = [0]
+    parameters_distributions = [{'weights': [0.0375, 0.0875, 0.0875, 0.0375, 0.0750, 0.1750,
+                                             0.1750, 0.0750, 0.0375, 0.0875, 0.0875, 0.0375],
+                                 'domain_random': domain_random}]
+    optimize_only_posterior_means = [False]
+    start_optimize_posterior_means = [0]
 
     #multi_task branin
 
@@ -595,128 +595,128 @@ if __name__ == '__main__':
 
     ##citibike_mt
     ############################### Parameters needed
-    n1 = 4
-    n2 = 1
-
-    nDays = 365
-
-    nSets = 4
-
-    fil = "poissonDays.txt"
-    fil = "problems/citi_bike_mt/" + fil
-    poissonParameters = np.loadtxt(fil)
-
-    ###readData
-
-    poissonArray = [[] for i in xrange(nDays)]
-    exponentialTimes = [[] for i in xrange(nDays)]
-
-    for i in xrange(nDays):
-        fil = "daySparse" + "%d" % i + "ExponentialTimesNonHom.txt"
-        fil2 = os.path.join("problems/citi_bike_mt/SparseNonHomogeneousPP2", fil)
-        poissonArray[i].append(np.loadtxt(fil2))
-
-        fil = "daySparse" + "%d" % i + "PoissonParametersNonHom.txt"
-        fil2 = os.path.join("problems/citi_bike_mt/SparseNonHomogeneousPP2", fil)
-        exponentialTimes[i].append(np.loadtxt(fil2))
-
-    numberStations = 329
-    Avertices = [[]]
-    for j in range(numberStations):
-        for k in range(numberStations):
-            Avertices[0].append((j, k))
-
-    with open('problems/citi_bike_mt/json.json') as data_file:
-        data = json.load(data_file)
-
-    f = open('problems/citi_bike_mt/' + str(4) + "-cluster.txt", 'r')
-    cluster = eval(f.read())
-    f.close()
-
-    bikeData = np.loadtxt("problems/citi_bike_mt/bikesStationsOrdinalIDnumberDocks.txt", skiprows=1)
-
-    TimeHours = 4.0
-    numberBikes = 6000
-
-    poissonParameters *= TimeHours
-
-    ###upper bounds for X
-    upperX = np.zeros(n1)
-    temBikes = bikeData[:, 2]
-    for i in xrange(n1):
-        temp = cluster[i]
-        indsTemp = np.array([a[0] for a in temp])
-        upperX[i] = np.sum(temBikes[indsTemp])
-
-
-    ##weights of w
-    def computeProbability(w, parLambda, nDays):
-        probs = poisson.pmf(w, mu=np.array(parLambda))
-        probs *= (1.0 / nDays)
-        return np.sum(probs)
-
-
-    L = 3829
-    M = 4010
-    wTemp = np.array(range(L, M))
-    probsTemp = np.zeros(M - L)
-    for i in range(M - L):
-        probsTemp[i] = computeProbability(wTemp[i], poissonParameters, nDays)
-
-    n_tasks = M - L
-    ###################
-    dim_x = [3]
-    bounds_domain_x = [[(0, upperX[0]), (0, upperX[1]), (0, upperX[2])]]
-    simplex_domain = [numberBikes]
-
-    problem_name = ['citi_bike_mt']
-    training_name = [None]
-    type_kernel = [[PRODUCT_KERNELS_SEPARABLE, MATERN52_NAME, TASKS_KERNEL_NAME]]
-    dimensions = [[4, 3, n_tasks]]
-    bounds_domain = [[[0, upperX[0]], [0, upperX[1]], [0, upperX[2]], range(n_tasks)]]
-    n_training = [50]
-    random_seed = range(1, 501)
-    n_specs = len(random_seed)
-    type_bounds = [[0, 0, 0, 1]]
-    x_domain = [[0, 1, 2]]
-    number_points_each_dimension = [[6, 6, 6]]
-    mle = [False]
-    distribution = [WEIGHTED_UNIFORM_FINITE]
-    parallel = [True]
-    thinning = [10]
-    n_burning = [500]
-    max_steps_out = [1000]
-    n_iterations = [21]
-    same_correlation = [True]
-    debug = [False]
-    number_points_each_dimension_debug = [[10, 10, 10, 10, 10, 10]]
-    monte_carlo_sbo = [True]
-    n_samples_mc = [5]
-    n_restarts_mc = [5]
-    n_best_restarts_mc = [0]
-    factr_mc = [1e12]
-    maxiter_mc = [10]
-    n_restarts = [100]
-    n_best_restarts = [10]
-    use_only_training_points = [True]
-    method_optimization = [MULTI_TASK_METHOD]
-    n_samples_parameters = [5]
-    n_restarts_mean = [100]
-    n_best_restarts_mean = [10]
-    method_opt_mc = [LBFGS_NAME]
-    maxepoch = [50]
-    n_samples_parameters_mean = [5]
-    maxepoch_mean = [50]
-    threshold_sbo = [0.001]
-    parallel_training = [False]
-    n_sampless = [5]
-    domain_random = [[i] for i in range(n_tasks)]
-    parameters_distributions = [{'weights': list(probsTemp),
-                                 'domain_random': domain_random}]
-
-    optimize_only_posterior_means = [False]
-    start_optimize_posterior_means = [0]
-    noises = [True]
+    # n1 = 4
+    # n2 = 1
+    #
+    # nDays = 365
+    #
+    # nSets = 4
+    #
+    # fil = "poissonDays.txt"
+    # fil = "problems/citi_bike_mt/" + fil
+    # poissonParameters = np.loadtxt(fil)
+    #
+    # ###readData
+    #
+    # poissonArray = [[] for i in xrange(nDays)]
+    # exponentialTimes = [[] for i in xrange(nDays)]
+    #
+    # for i in xrange(nDays):
+    #     fil = "daySparse" + "%d" % i + "ExponentialTimesNonHom.txt"
+    #     fil2 = os.path.join("problems/citi_bike_mt/SparseNonHomogeneousPP2", fil)
+    #     poissonArray[i].append(np.loadtxt(fil2))
+    #
+    #     fil = "daySparse" + "%d" % i + "PoissonParametersNonHom.txt"
+    #     fil2 = os.path.join("problems/citi_bike_mt/SparseNonHomogeneousPP2", fil)
+    #     exponentialTimes[i].append(np.loadtxt(fil2))
+    #
+    # numberStations = 329
+    # Avertices = [[]]
+    # for j in range(numberStations):
+    #     for k in range(numberStations):
+    #         Avertices[0].append((j, k))
+    #
+    # with open('problems/citi_bike_mt/json.json') as data_file:
+    #     data = json.load(data_file)
+    #
+    # f = open('problems/citi_bike_mt/' + str(4) + "-cluster.txt", 'r')
+    # cluster = eval(f.read())
+    # f.close()
+    #
+    # bikeData = np.loadtxt("problems/citi_bike_mt/bikesStationsOrdinalIDnumberDocks.txt", skiprows=1)
+    #
+    # TimeHours = 4.0
+    # numberBikes = 6000
+    #
+    # poissonParameters *= TimeHours
+    #
+    # ###upper bounds for X
+    # upperX = np.zeros(n1)
+    # temBikes = bikeData[:, 2]
+    # for i in xrange(n1):
+    #     temp = cluster[i]
+    #     indsTemp = np.array([a[0] for a in temp])
+    #     upperX[i] = np.sum(temBikes[indsTemp])
+    #
+    #
+    # ##weights of w
+    # def computeProbability(w, parLambda, nDays):
+    #     probs = poisson.pmf(w, mu=np.array(parLambda))
+    #     probs *= (1.0 / nDays)
+    #     return np.sum(probs)
+    #
+    #
+    # L = 3829
+    # M = 4010
+    # wTemp = np.array(range(L, M))
+    # probsTemp = np.zeros(M - L)
+    # for i in range(M - L):
+    #     probsTemp[i] = computeProbability(wTemp[i], poissonParameters, nDays)
+    #
+    # n_tasks = M - L
+    # ###################
+    # dim_x = [3]
+    # bounds_domain_x = [[(0, upperX[0]), (0, upperX[1]), (0, upperX[2])]]
+    # simplex_domain = [numberBikes]
+    #
+    # problem_name = ['citi_bike_mt']
+    # training_name = [None]
+    # type_kernel = [[PRODUCT_KERNELS_SEPARABLE, MATERN52_NAME, TASKS_KERNEL_NAME]]
+    # dimensions = [[4, 3, n_tasks]]
+    # bounds_domain = [[[0, upperX[0]], [0, upperX[1]], [0, upperX[2]], range(n_tasks)]]
+    # n_training = [50]
+    # random_seed = range(1, 501)
+    # n_specs = len(random_seed)
+    # type_bounds = [[0, 0, 0, 1]]
+    # x_domain = [[0, 1, 2]]
+    # number_points_each_dimension = [[6, 6, 6]]
+    # mle = [False]
+    # distribution = [WEIGHTED_UNIFORM_FINITE]
+    # parallel = [True]
+    # thinning = [10]
+    # n_burning = [500]
+    # max_steps_out = [1000]
+    # n_iterations = [21]
+    # same_correlation = [True]
+    # debug = [False]
+    # number_points_each_dimension_debug = [[10, 10, 10, 10, 10, 10]]
+    # monte_carlo_sbo = [True]
+    # n_samples_mc = [5]
+    # n_restarts_mc = [5]
+    # n_best_restarts_mc = [0]
+    # factr_mc = [1e12]
+    # maxiter_mc = [10]
+    # n_restarts = [100]
+    # n_best_restarts = [10]
+    # use_only_training_points = [True]
+    # method_optimization = [MULTI_TASK_METHOD]
+    # n_samples_parameters = [5]
+    # n_restarts_mean = [100]
+    # n_best_restarts_mean = [10]
+    # method_opt_mc = [LBFGS_NAME]
+    # maxepoch = [50]
+    # n_samples_parameters_mean = [5]
+    # maxepoch_mean = [50]
+    # threshold_sbo = [0.001]
+    # parallel_training = [False]
+    # n_sampless = [5]
+    # domain_random = [[i] for i in range(n_tasks)]
+    # parameters_distributions = [{'weights': list(probsTemp),
+    #                              'domain_random': domain_random}]
+    #
+    # optimize_only_posterior_means = [False]
+    # start_optimize_posterior_means = [0]
+    # noises = [True]
 
 
 
