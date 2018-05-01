351f76c04474441ab2d84f6cb20e7ab295c3de50
Saul Toscano Palmerin <toscano.saul@gmail.com>
2018-05-01 01:52:37 -0400

    run_replications_policies.sh

--- run_replications_policies.sh
+++ run_replications_policies.sh
@@ -5,7 +5,9 @@ n_restarts=$4
 lr=$5
 std=$6
 
-for i in {$lw..$up}
+rm -r data/gp_models/$problem 
+for i in $(seq $lw $up)
 do
+    echo $i
     screen -d -m python -m multi_start.script_run_policies_from_beginning $i $n_restarts $std $lr approx_lipschitz 0 100 $problem
 done
